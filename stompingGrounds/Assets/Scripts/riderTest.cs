f302dc50b337f0072e0999d9f7fcb2c89df0e761
Cameron Bass <10748722@my.uvu.edu>
2021-08-25 10:40:52 -0600

    Testing Settings and editor

---
+++ stompingGrounds/Assets/Scripts/riderTest.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class riderTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("you're alive");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
