7b659b2330cea1f64c2c903610b6864e5fe1083c
Cameron <Cameronjudd98@gmail.com>
2021-08-31 15:02:19 -0600

    Auto stash before merge of "main" and "origin/main"

--- stompingGrounds/Assets/Scripts/riderTest.cs
+++ stompingGrounds/Assets/Scripts/riderTest.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class riderTest : MonoBehaviour
+public class RiderTest : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
