5e59d3c53ae174ef5d64f1ad12e0c900c5579dc7
Cameron Bass <10748722@uvu.edu>
2022-01-13 11:00:51 -0700

    Understanding Overrides

--- DGMPractice/Assets/Scripts/GrapplePowerUp.cs
+++ DGMPractice/Assets/Scripts/GrapplePowerUp.cs
@@ -2,4 +2,10 @@ public class GrapplePowerUp : PowerupData {
     protected override void PrintName() {
         print("Grapple Powerup");
     }
+
+    protected override void ApplyToPlayer() {
+        base.ApplyToPlayer();
+        
+        print("Give player 1 charge of grapple");
+    }
 }
