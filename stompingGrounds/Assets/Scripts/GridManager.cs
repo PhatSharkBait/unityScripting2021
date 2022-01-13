eb2999ee6222733e6c9fc7088fa4d7062e670f65
Cameron Bass <Cameronjudd98@gmail.com>
2022-01-12 17:39:27 -0700

    Working on grid spaces list

--- stompingGrounds/Assets/Scripts/GridManager.cs
+++ stompingGrounds/Assets/Scripts/GridManager.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GridManager : MonoBehaviour
@@ -8,20 +10,32 @@ public class GridManager : MonoBehaviour
     public GameObject ObjectToSpawn;
     public Vector3 offset;
 
-    private void generateGrid()
-    {
+    private List<GameObject> _gridSpaces;
+    private GameObject newObject;
+
+    private void GenerateGrid() {
+        int k = 0;
         for (int i = 0; i < Columns; i++)
         {
             for (int j = 0; j < Rows; j++)
             {
-                var newObject = Instantiate(ObjectToSpawn, new Vector3(i, j, 0) + offset, Quaternion.identity);
-                newObject.GetComponent<SpriteRenderer>().enabled = false;
+                _gridSpaces.Add(Instantiate(ObjectToSpawn, new Vector3(i, j, 0) + offset, Quaternion.identity));
+                //newObject = Instantiate(ObjectToSpawn, new Vector3(i, j, 0) + offset, Quaternion.identity);
+                //newObject.GetComponent<GridData>().gridID = k;
+                //k++;
+                //print(newObject);
+                //_gridSpaces.Add(newObject);
+                
             }
+        }
+
+        foreach (var space in _gridSpaces) {
+            space.GetComponent<GridData>().GenerateNeighbors(_gridSpaces);
         }
     }
 
     private void Start()
     {
-        generateGrid();
+        GenerateGrid();
     }
 }
