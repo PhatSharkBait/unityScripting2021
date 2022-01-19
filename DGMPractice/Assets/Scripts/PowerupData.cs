8e3863259b960020044b25813d3af6ec6947c322
Cameron Bass <Cameronjudd98@gmail.com>
2022-01-18 20:38:46 -0700

    Understanding Interfaces

--- DGMPractice/Assets/Scripts/PowerupData.cs
+++ DGMPractice/Assets/Scripts/PowerupData.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 
-public class PowerupData : MonoBehaviour {
+[RequireComponent(typeof(BoxCollider))]
+public class PowerupData : MonoBehaviour, IPowerUp {
     protected int powerID;
 
     private void Start() {
+        collider = GetComponent<BoxCollider>();
         PrintName();
         ApplyToPlayer();
+        OnSpawn();
     }
 
     protected virtual void PrintName() {
@@ -15,4 +18,23 @@ public class PowerupData : MonoBehaviour {
     protected virtual void ApplyToPlayer() {
         print("get player");
     }
+
+    public new BoxCollider collider { get; set; }
+    public float PowerupID { get; set; }
+
+    public void OnPickup() {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnSpawn() {
+        collider.center = new Vector3(0, 0, 0);
+        collider.size = new Vector3(1, 1, 1);
+    }
+}
+
+public interface IPowerUp {
+    BoxCollider collider { get; set; }
+    public float PowerupID { get; set; }
+    void OnPickup();
+    void OnSpawn();
 }
