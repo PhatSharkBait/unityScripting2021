633fb302f572ef5ed0caf6d7c6985d307da4bd74
Cameron Bass <Cameronjudd98@gmail.com>
2022-02-22 11:05:51 -0700

    Understanding Overloading

--- UnityPractice/Assets/Scripts/MethodOverloading.cs
+++ UnityPractice/Assets/Scripts/MethodOverloading.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
 [CreateAssetMenu]
 public class MethodOverloading : ScriptableObject {
-    public void ChangeValue(int num) {
-        var newNum = num + 2;
+    public void ChangeValue(int intNum) {
+        var newNum = intNum + 2;
     }
 
-    public void ChangeValue(float num) {
-        var newNum = num + 10.5f;
+    public void ChangeValue(float floatNum) {
+        var newNum = floatNum + 10.5f;
+    }
+    
+    public void ChangeValue(int intNum, float floatNum) {
+        float newNum = intNum + 2;
+        newNum = floatNum * newNum;
     }
 }
