6ef6a3f63a849b8f410e0686f85b0ef4f5ce376a
Cameron Bass <Cameronjudd98@gmail.com>
2022-03-25 16:35:27 -0600

    Fixed flickering Lights

--- BasementMaze/Assets/Scripts/GameController.cs
+++ BasementMaze/Assets/Scripts/GameController.cs
@@ -6,7 +6,7 @@ public class GameController : MonoBehaviour {
     private MazeConstructor _mazeConstructor;
     void Start() {
         _mazeConstructor = GetComponent<MazeConstructor>();
-        _mazeConstructor.GenerateNewMaze(21, 21);
+        _mazeConstructor.GenerateNewMaze(15, 15);
     }
 
 }
