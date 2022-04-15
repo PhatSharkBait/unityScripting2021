bf134e769a02d775271d106366e978fc794ef13e
Cameron Bass <Cameronjudd98@gmail.com>
2022-04-15 13:40:22 -0600

    Fixed null reference error

--- BasementMaze/Assets/Scripts/SetTextFromInt.cs
+++ BasementMaze/Assets/Scripts/SetTextFromInt.cs
@@ -6,12 +6,15 @@ public class SetTextFromInt : MonoBehaviour {
 
     public IntDataSO intDataSo;
 
-    private void Start() {
+    private void Awake() {
         _textObj = gameObject.GetComponent<Text>();
         SetTextFromIntData();
     }
 
     public void SetTextFromIntData() {
-        _textObj.text = intDataSo.value.ToString();
+        _textObj.text = intDataSo.value.ToString() + " remain";
+        if(intDataSo.value == 1){
+            _textObj.text = intDataSo.value.ToString() + " remains";
+        }
     }
 }
